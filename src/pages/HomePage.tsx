57212ce8c9f96e3257b9ab22b058ea0e14fafe3b
Nygaaard <nygardandreas@hotmail.com>
2025-03-03 14:40:35 +0100

    Created post interface and getData function

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
+import { PostSchema } from "../types/PostInterface";
+
 const HomePage = () => {
+  const [posts, setPosts] = useState<PostSchema[] | []>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:3000/posts");
+
+      if (!response.ok) {
+        throw Error("Något gick fel...");
+      } else {
+        const data = await response.json();
+
+        //Set posts
+        setPosts(data);
+      }
+    } catch {
+      setError("Något gick fel vid hämtning... ");
+    } finally {
+      //Avbryt laddning
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h2>Startsidan</h2>
+    <div className="app-container">
+      <h2>Alla inlägg</h2>
+      {loading && <p className="loading">Laddar...</p>}
+      {error && <p className="error-message">{error}</p>}
+
+      <div className="posts">
+        {posts.map((post) => (
+          <h3>{post.title}</h3>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/types/PostInterface.ts
@@ -0,0 +1,11 @@
+export interface PostSchema {
+    id: number;
+    title: string;
+    content: string;
+    createdAt: string;
+}
+
+export interface ErrorData {
+    title?: string;
+    content?: string;
+}
